ac5561279cc0a21101eb17ea8f09ffef00798df6
bowang <boscodtt@gmail.com>
2018-03-09 13:58:28 +0800

    added windows date time format as reference

--- clipboard-backup.bat
+++ clipboard-backup.bat
@@ -1,3 +1,7 @@
+::short date: dd/MM/yyyy
+::long time: HH:mm:ss
+::echo %time% -> 13:54:30.40
+
 mkdir C:\temp\%date:~-4,4%%date:~-7,2%%date:~-10,2%
 set currentTime=%time::=_%
 set currentTime=%currentTime:.=_%
