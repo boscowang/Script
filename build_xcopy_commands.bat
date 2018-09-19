6c81e4c7977d5705a38853b65be89a4ef946fadc
wingkwong <wingkwong.code@gmail.com>
2018-09-19 21:08:33 +0800

    supported folder structure copy

--- build_xcopy_commands.bat
+++ build_xcopy_commands.bat
@@ -1,14 +1,27 @@
 @echo off
+SETLOCAL  ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 :: FILE WITH LIST OF FILES TO BE COPIED
-SET INPUT=C:\temp\in.txt
+SET INPUT=C:\tmp\xcopy_in.txt
+:: C:\tmp\root\file1.txt
+:: C:\tmp\root\folder2\file2.txt
+:: C:\tmp\root\folder3\file3.txt
+:: C:\tmp\root\folder4\file4.txt
+:: ROOT OF INPUT PATH
+SET INPUT_ROOT=C:\tmp\root\
 :: TARGET LOCATION OF FILES TO BE COPIED TO
-SET TARGET=C:\temp\target\
+SET TARGET_ROOT=C:\tmp\target\
 :: FILE WITH LIST OF XCOPY COMMANDS BASED ON INPUT
-SET OUTPUT=C:\temp\out.cmd
+SET OUTPUT=C:\tmp\out.cmd
+:: C:\tmp\target\file1.txt
+:: C:\tmp\target\folder2\file2.txt
+:: C:\tmp\target\folder3\file3.txt
+:: C:\tmp\target\folder4\file4.txt
 
 IF EXIST %OUTPUT% DEL %OUTPUT%
 FOR /F "TOKENS=* DELIMS=" %%E IN ('TYPE %INPUT%') DO (
-	ECHO XCOPY /F /Y "%%E" "%TARGET%" >> %OUTPUT%
+	SET "INPUT_PATH=%%E"
+	SET "TARGET_PATH=!INPUT_PATH:%INPUT_ROOT%=%TARGET_ROOT%!"
+	ECHO XCOPY /F /Y "%%E" "!TARGET_PATH!*" >> %OUTPUT%
 )
-
+ENDLOCAL
