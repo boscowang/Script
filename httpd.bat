d6bf5c75259b558f800800efed2bc7b41c7c28ed
bowang <boscodtt@gmail.com>
2018-09-30 19:42:49 +0800

    added apache download link

--- httpd.bat
+++ httpd.bat
@@ -1,2 +1,3 @@
+:: (new-object System.Net.WebClient).DownloadFile('https://home.apache.org/~steffenal/VC15/binaries/httpd-2.4.35-win64-VC15.zip','C:\temp\httpd-2.4.35-win64-VC15.zip')
 title httpd
 C:\Apache24\bin\httpd
